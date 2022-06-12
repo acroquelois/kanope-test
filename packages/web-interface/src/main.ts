748d710d975bb58e0789ff9a41b23e8c39e6ae55
croquelois <croquelois.adrien@gmail.com>
2022-06-12 16:32:20 +0200

    :wrench: add quasar config

--- packages/web-interface/src/main.ts
+++ packages/web-interface/src/main.ts
@@ -1,4 +1,19 @@
 import { createApp } from 'vue'
+import { Quasar } from 'quasar'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// Import icon libraries
+import '@quasar/extras/material-icons/material-icons.css'
+
+// Import Quasar css
+import 'quasar/src/css/index.sass'
+
+// Assumes your root component is App.vue
+// and placed in same folder as main.js
+import App from './App.vue'
+
+createApp(App)
+    .use(Quasar, {
+        plugins: {}, // import Quasar plugins and add here
+    })
+    .mount('#app')
